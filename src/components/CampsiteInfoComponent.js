7b8571d3d88b9c689ab429538803780ea5950231
Cameron Rosenberger <crosenberger17@gmail.com>
2021-07-03 13:26:37 -0700

    Week 4 Workshop Comment Modal

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Button, Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Modal, ModalBody, ModalHeader, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 
 function RenderCampsite({campsite}) {
@@ -17,9 +21,84 @@ function RenderCampsite({campsite}) {
         )
 }
 
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };        
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+    
+
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });        
+    }
+
+    handleSubmit=(values) => {
+        this.toggleModal();
+        console.log("The current state is" + JSON.stringify(values))
+        alert("The current state is" + JSON.stringify(values))
+    }
+
+
+    render() {
+    return (
+        <React.Fragment>
+            <Button outline onClick={this.toggleModal}> <i className='fa fa-pencil fa-lg'/>Submit Comment</Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+            <ModalBody>
+                <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                <div className="form-group">
+                <Label htmlFor="rating">Rating</Label>
+                    <Control.select className="form-control" model=".rating" id="rating" name="rating">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Control.select>
+                </div>
+                <div className="form-group">
+                <Label htmlFor="author">Your Name</Label>
+                    <Control.text className="form-control" model=".author" id="author" name="author" placeholder="Your Name"                                         validators={{
+                        required,
+                        minLength: minLength(2),
+                        maxLength: maxLength(15) }}
+                    />
+                    <Errors
+                        className="text-danger"
+                        model=".author"
+                        show="touched"
+                        component="div"
+                        messages={{
+                        required: 'Required',
+                        minLength: 'Must be at least 2 characters',
+                        maxLength: 'Must be at 15 characters or less'
+                        }}
+                    />
+                </div>
+                <div className="form-group">
+                <Label htmlFor="comment">Comment</Label>                    
+                    <Control.textarea className="form-control" model =".text" id="text" name="text" rows="6" />
+                </div>
+                <Button type="submit" color="primary">Submit</Button> 
+                </LocalForm>
+                </ModalBody>
+            </Modal>
+        </React.Fragment>
+    )
+    }
+}
+
 function RenderComments({comments}) {
     if(comments) {
         return(
+            
             <div className='col-md-5 m-1'>
                 <h4>Comments</h4> 
                 {comments.map( comment => {
@@ -29,9 +108,12 @@ function RenderComments({comments}) {
                             {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                             </p>
                         </div>
+                        
                     );
                 })}
-            </div>
+                <CommentForm />
+            </div>           
+            
         );
     }
     return <div />
@@ -59,7 +141,7 @@ function RenderComments({comments}) {
             );
         }
         return <div />            
-    }             
+    }    
     
 
  
