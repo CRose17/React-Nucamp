635fd67ad1f91d52384d8210ba446ef229536c76
Cameron Rosenberger <crosenberger17@gmail.com>
2021-07-07 13:58:29 -0700

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -41,7 +41,7 @@ class CommentForm extends Component {
 
     handleSubmit=(values) => {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
 
@@ -95,7 +95,7 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if(comments) {
         return(
             
@@ -111,7 +111,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>           
             
         );
@@ -157,7 +157,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                             comments ={props.comments}
-                            addComment ={props.addComment}
+                            postComment ={props.postComment}
                             campsiteId={props.campsite.id}
                          />
                     </div>
